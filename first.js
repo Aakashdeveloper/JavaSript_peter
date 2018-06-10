ec70d006c1ab8ff841c9f273937283bac18f04e3
AAKASH HANDA <AAKASH HANDA>
2018-06-10 08:27:44 +0530

    A

---
+++ first.js
@@ -0,0 +1,78 @@
+Data Types
+
+String = "jhju"  "89"
+Number= 67890998
+Boolean= true/false
+
+Es5/Es6=====>Es8
+=========================================================
+var a = "peter"
+undefined
+a
+"peter"
+var b = 10
+undefined
+b
+10
+var c = true
+undefined
+c
+true
+var d = "10"
+undefined
+d
+"10"
+typeof(d)
+"string"
+typeof(c)
+"boolean"
+typeof(b)
+"number"
+typeof(a)
+"string"
+=========================================================
+var name = "peter"
+undefined
+var sub = " javascript"
+undefined
+name+sub
+"peter javascript"
+var first = 1
+undefined
+var second = 2
+undefined
+first + second
+3
+name.length
+5
+name.toUpperCase()
+"PETER"
+name.toLowerCase()
+"peter"
+var mytext = " i am learning javascript"
+undefined
+mytext.replace("javascript", "Python")
+" i am learning Python"
+mytext.trim()
+"i am learning javascript"
+var mytext = "     i am learning javascript     "
+undefined
+mytext.trim()
+"i am learning javascript"
+mytext.charAt(0)
+" "
+var myresult = mytext.trim()
+undefined
+myresult
+"i am learning javascript"
+myresult.charAt(0)
+"i"
+myresult.slice(1)
+" am learning javascript"
+myresult.slice(3)
+"m learning javascript"
+"A"+myresult.slice(1)
+"A am learning javascript"
+myresult.charAt(0).toUpperCase()+myresult.slice(1)
+"I am learning javascript"
+====================================================================
